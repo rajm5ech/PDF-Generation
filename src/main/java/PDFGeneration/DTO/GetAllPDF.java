8dda621d60e585fc459cbcc03384440068cef5f7
rajnish <rajm5ech@gmail.com>
2024-06-17 17:29:26 +0530

    Removed unused imports

--- src/main/java/PDFGeneration/DTO/GetAllPDF.java
+++ src/main/java/PDFGeneration/DTO/GetAllPDF.java
@@ -1,12 +1,7 @@
 package PDFGeneration.DTO;
 
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Date;
 @Getter
 @Setter
 public class GetAllPDF {
