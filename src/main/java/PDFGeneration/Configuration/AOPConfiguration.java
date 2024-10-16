33e25bb056b7049a1058bde4c1324f458ad3f4e8
rajnish <rajm5ech@gmail.com>
2024-10-17 00:54:12 +0530

    AOP Config - Centralized Exception Handling & Propagation Up The Call Stack

--- src/main/java/PDFGeneration/Configuration/AOPConfiguration.java
+++ src/main/java/PDFGeneration/Configuration/AOPConfiguration.java
@@ -2,6 +2,7 @@ package PDFGeneration.Configuration;
 
 import PDFGeneration.Exception.UserIdInvalid;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,24 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class AOPConfiguration {
-
-    @AfterThrowing(pointcut = "execution(* PDFGeneration.Controller.PDFGenerationController.getAllPdf(..))", throwing = "ex")
-    public void centralizeExceptionHandling(NullPointerException ex){
-        log.info("Exception is caught"+ex.getMessage());
+    @AfterThrowing(pointcut = "execution(* PDFGeneration.Repository.PDFRepository.*(..))", throwing = "ex")
+    public void centralizeExceptionHandlingRepository(JoinPoint joinPoint , ArithmeticException ex){
+        log.error("Exception is caught :: {} at method :: {}" , ex.getMessage() , joinPoint);
         throw new UserIdInvalid("User is unauthorized to visit the page");
     }
-}
+    @AfterThrowing(pointcut = "execution(* PDFGeneration.Controller.PDFGenerationController.*(..))", throwing = "ex")
+    public void centralizeExceptionHandling(JoinPoint joinPoint , UserIdInvalid ex){
+        log.info("Exception is caught :: {} at method :: {}" , ex.getMessage() , joinPoint);
+        throw new UserIdInvalid("User is unauthorized to visit the page");
+    }
+    @AfterThrowing(pointcut = "execution(* PDFGeneration.Service.PDFService.*(..))", throwing = "ex")
+    public void centralizeExceptionHandlingService(JoinPoint joinPoint , UserIdInvalid ex) {
+        log.error("Exception is caught :: {} at method :: {}", ex.getMessage(), joinPoint);
+        throw new UserIdInvalid("User is unauthorized to visit the page");
+    }
+
+
+    }
+
+
+
